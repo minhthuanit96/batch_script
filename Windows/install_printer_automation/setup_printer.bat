dff6c999f4fab688d23c531d4975290eb200fd52
minhthuanit96 <minhthuanit96@gmail.com>
2023-12-15 17:26:20 +0700

    add function roll back main menu

--- Windows/install_printer_automation/setup_printer.bat
+++ Windows/install_printer_automation/setup_printer.bat
@@ -1,5 +1,5 @@
 @echo off
-TITLE "INSTALL PRINTER FOR WINDOWS"
+TITLE "INSTALL PRINTER FOR WINDOWS"                                                                                                                                       
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 if '%errorlevel%' NEQ '0' (
     echo  Run CMD as Administrator...
@@ -20,6 +20,15 @@ REM Go UAC to get Admin privileges
     pushd "%CD%"
     CD /D "%~dp0"
 REM ========================================================================================================================================
+echo " ____  ____     ___  _____   ___  ____   ______    ___  ___        ____   __ __      ______  __ __  __ __   ____  ____       ____  ______ ";
+echo "|    \|    \   /  _]/ ___/  /  _]|    \ |      |  /  _]|   \      |    \ |  |  |    |      ||  |  ||  |  | /    ||    \     |    ||      |";
+echo "|  o  )  D  ) /  [_(   \_  /  [_ |  _  ||      | /  [_ |    \     |  o  )|  |  |    |      ||  |  ||  |  ||  o  ||  _  |     |  | |      |";
+echo "|   _/|    / |    _]\__  ||    _]|  |  ||_|  |_||    _]|  D  |    |     ||  ~  |    |_|  |_||  _  ||  |  ||     ||  |  |     |  | |_|  |_|";
+echo "|  |  |    \ |   [_ /  \ ||   [_ |  |  |  |  |  |   [_ |     |    |  O  ||___, |      |  |  |  |  ||  :  ||  _  ||  |  |     |  |   |  |  ";
+echo "|  |  |  .  \|     |\    ||     ||  |  |  |  |  |     ||     |    |     ||     |      |  |  |  |  ||     ||  |  ||  |  |     |  |   |  |  ";
+echo "|__|  |__|\_||_____| \___||_____||__|__|  |__|  |_____||_____|    |_____||____/       |__|  |__|__| \__,_||__|__||__|__|    |____|  |__|  ";
+echo "                                                                                                                                             
+REM ========================================================================================================================================
 set folderdriver=C:\temp\driver
 net use \\192.168.1.10\ibasic /user:minhthuan 123
 REM ========================================================================================================================================
@@ -30,19 +39,19 @@ setlocal
 title Main Menu
 echo.
 echo %appversion%
-echo    ========================================================
-echo    [1] Install Brother MFC-L2701DW                : Press 1
-echo    [2] Install Brother HL-L2360DW                 : Press 2
-echo    [3] Install Gestetner MP 5054 PCL 6            : Press 3
-echo    [4] Install Brother HL-L8350CDW series         : Press 4
-echo    [5] Exit                                       : Press 5
-echo    ========================================================
+echo    ==================================================================
+echo    [1] Install Brother MFC-L2701DW (IT's PRINTER)           : Press 1
+echo    [2] Install Brother HL-L2360DW  (PRODUCTION's PRINTER)   : Press 2
+echo    [3] Install Gestetner MP 5054 (PHOTOCOPY)                : Press 3
+echo    [4] Install Brother HL-L8350CDW (COLOR PRINTER)          : Press 4
+echo    [5] Exit                                                 : Press 5
+echo    ==================================================================
 Choice /N /C 12345 /M " Your choice is :"
-if %ERRORLEVEL% == 5 goto exit
-if %ERRORLEVEL% == 4 call :Install-Brother-HL-L8350CDW-series
-if %ERRORLEVEL% == 3 call :Install-Gestetner-MP-5054-PCL-6
-if %ERRORLEVEL% == 2 call :Install-Brother-HL-L2360DW
-if %ERRORLEVEL% == 1 call :Install-Brother-MFC-L2701DW
+if %ERRORLEVEL% == 5 call :timeout & goto exits
+if %ERRORLEVEL% == 4 call :Install-Brother-HL-L8350CDW-series & goto main
+if %ERRORLEVEL% == 3 call :Install-Gestetner-MP-5054-PCL-6 & goto main
+if %ERRORLEVEL% == 2 call :Install-Brother-HL-L2360DW & goto main
+if %ERRORLEVEL% == 1 call :Install-Brother-MFC-L2701DW & goto main
 endlocal
 goto end
 REM ========================================================================================================================================
@@ -63,8 +72,8 @@ Cscript "%folderdriver%\Prndrvr.vbs" -a -m %printername% -i %folderdriver%\BRPRM
 :: Add The new printer
 Cscript "%folderdriver%\Prnmngr.vbs" -a -p "Brother MFC-L2701DW" -m %printername% -r IP_%ipprinter%
 rd C:\temp /s /q
+cls
 endlocal
-call :timeout
 goto :EOF
 REM =========================================================================================================================================
 echo The process is still active....
@@ -84,8 +93,8 @@ Cscript "%folderdriver%\Prndrvr.vbs" -a -m %printername% -i %folderdriver%\BROHL
 :: Add The new printer
 Cscript "%folderdriver%\Prnmngr.vbs" -a -p "Brother HL-L2360D series" -m %printername% -r IP_%ipprinter%
 rd C:\temp /s /q
+cls
 endlocal
-call :timeout
 goto :EOF
 REM =========================================================================================================================================
 echo The process is still active....
@@ -105,8 +114,8 @@ Cscript "%folderdriver%\Prndrvr.vbs" -a -m %printername% -i %folderdriver%\oemse
 :: Add The new printer
 Cscript "%folderdriver%\Prnmngr.vbs" -a -p "Gestetner MP 5054 PCL 6" -m %printername% -r IP_%ipprinter%
 rd C:\temp /s /q
+cls
 endlocal
-call :timeout
 goto :EOF
 REM =========================================================================================================================================
 echo The process is still active....
@@ -126,12 +135,16 @@ Cscript "%folderdriver%\Prndrvr.vbs" -a -m %printername% -i %folderdriver%\BROCH
 :: Add The new printer
 Cscript "%folderdriver%\Prnmngr.vbs" -a -p "Brother HL-L8350CDW series" -m %printername% -r IP_%ipprinter%
 rd C:\temp /s /q
+cls
 endlocal
-call :timeout
 goto :EOF
 REM =========================================================================================================================================
 :timeout
 cls
 echo Installation Completed
-timeout /t 2 /nobreak>nul
+echo " ______ __  __  ___  __  __ __ __    _  _   ___   __ __     ____   ___   ____     __ __  __  __ __  __   ___ "
+echo " | || | ||  || // \\ ||\ || || //    \\//  // \\  || ||    ||     // \\  || \\    || || (( \ || ||\ ||  // \\"
+echo "   ||   ||==|| ||=|| ||\\|| ||<<      )/  ((   )) || ||    ||==  ((   )) ||_//    || ||  \\  || ||\\|| (( ___"
+echo "   ||   ||  || || || || \|| || \\    //    \\_//  \\_//    ||     \\_//  || \\    \\_// \_)) || || \||  \\_||"                                                                                                                                                                                                                                                ";                                                                                                                    
+timeout /t 5 /nobreak>nul
 exit
